bb3ba6a6132abe78f8cb4d2747ac913743082f66
Alika Iwe <alikaiwe@gmail.com>
2024-01-10 22:31:06 +0100

    created the User Management Controller. In it, i created the method to create a new user on the platform. Also created the stored procedure for it. Also is the Utility class for global services.

--- SoundShareHub/Controllers/UserManagementController.cs
+++ SoundShareHub/Controllers/UserManagementController.cs
@@ -25,6 +25,7 @@
 */
 
 using SoundShareHub.Models;
+using SoundShareHub.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -34,10 +35,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
+//using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace SoundShareHub.Controllers
 {
-    public class UserManagementController
+    [ApiController]
+    [Route("[controller]")]
+    public class UserManagementController : ControllerBase
     {
         string username = "";
         int UserID = 0;
@@ -47,6 +52,48 @@ namespace SoundShareHub.Controllers
         public UserManagementController(ConnectionStringProvider connectionStringProvider)
         {
             _connectionStringProvider = connectionStringProvider;
+        }
+
+        [Route("api/createnewuser")]
+        [HttpPost]
+        public String CreateUser([FromBody] UserManagement um)
+        {
+            var GetVerifiedDate = new Helper();
+            try
+            {
+                string connectionString = _connectionStringProvider.GetLocalConnectionString();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SOUNDSHAREHUB_CreateNewUser", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.Add("@Username", SqlDbType.NVarChar).Value = um.Username != null ? (object)um.Username : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Firstname", SqlDbType.NVarChar).Value = um.FirstName != null ? (object)um.FirstName : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Lastname", SqlDbType.NVarChar).Value = um.LastName != null ? (object)um.LastName : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Issuperuser", SqlDbType.Bit).Value = um.isSuperUser != false ? (object)um.isSuperUser : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Email", SqlDbType.NVarChar).Value = um.Email != null ? (object)um.Email : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Displayname", SqlDbType.NVarChar).Value = um.DisplayName != null ? (object)um.DisplayName : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Password", SqlDbType.NVarChar).Value = um.Password != null ? (object)um.Password : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Phoneno", SqlDbType.NVarChar).Value = um.PhoneNo != null ? (object)um.PhoneNo : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@dob", SqlDbType.DateTime).Value = GetVerifiedDate.verifyDate(um.Dob); // Replace with actual values
+                        command.Parameters.Add("@Lastipaddress", SqlDbType.NVarChar).Value = um.LastIPAddress != null ? (object)um.LastIPAddress : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Isdeleted", SqlDbType.Bit).Value = um.IsDeleted != false ? (object)um.IsDeleted : DBNull.Value; // Replace with actual values
+                        command.Parameters.Add("@Createdbyuserid", SqlDbType.Int).Value = 5; // Replace with actual values
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+                return "success";
+                //return Request.CreateResponse(HttpStatusCode.OK, new { Status = "Success", Message = "User inserted successfully." });
+            }
+            catch(Exception e) { }
+            return "Failed";
+            //return Request.CreateResponse(HttpStatusCode.InternalServerError,
+               // new { Status = "Fail", Message = "Failed to Create new user." });
         }
     }
 }
---
+++ SoundShareHub/Utilities/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlTypes;
+using System;
+
+namespace SoundShareHub.Utilities
+{
+    public class Helper
+    {
+        public DateTime verifyDate(DateTime a)
+        {
+            if (a < SqlDateTime.MinValue.Value || a > SqlDateTime.MaxValue.Value)
+            {
+                // Handle the case where a.PurchaseDate is out of range
+                return SqlDateTime.MinValue.Value;
+            }
+            return a;
+        }
+    }
+}
